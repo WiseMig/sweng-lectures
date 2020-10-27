751b0af8c8bdc6851a0e1f257dab923b5e6fa4c8
Alessandro Miguez <afmoreira@ufp.edu.pt>
2020-10-27 15:17:28 +0000

    Lógica e teste para Explicador

--- src/main/java/pt/ufp/info/esof/lectures/models/Explicador.java
+++ src/main/java/pt/ufp/info/esof/lectures/models/Explicador.java
@@ -1,8 +1,13 @@
 package pt.ufp.info.esof.lectures.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
 public class Explicador extends Utilizador{
     private String email;
     private final List<Cadeira> cadeiras=new ArrayList<>();
@@ -10,15 +15,37 @@ public class Explicador extends Utilizador{
     private final List<Explicacao> explicacoes=new ArrayList<>();
 
     public Explicacao adicionarExplicacao(Explicacao explicacao){
+        if(estaDisponivel(explicacao)&&!temMarcacaoPrevia(explicacao)){
+            this.explicacoes.add(explicacao);
+            explicacao.setExplicador(this);
+            return explicacao;
+        }
         return null;
     }
 
     private boolean estaDisponivel(Explicacao explicacao){
+        for(Disponibilidade disponibilidade:disponibilidades){
+            if(disponibilidade.estaDisponivel(explicacao)){
+                return true;
+            }
+        }
         return false;
     }
 
     private boolean temMarcacaoPrevia(Explicacao explicacao){
+        for(Explicacao explicacaoExistente:explicacoes){
+            if(explicacaoExistente.temMarcacaoPrevia(explicacao)){
+                return true;
+            }
+        }
         return false;
+    }
+
+    public void adicionaDisponibilidade(Disponibilidade disponibilidade){
+        if(!this.disponibilidades.contains(disponibilidade)){
+            this.disponibilidades.add(disponibilidade);
+            disponibilidade.setExplicador(this);
+        }
     }
 
 }
--- src/test/java/pt/ufp/info/esof/lectures/LecturesApplicationTests.java
+++ src/test/java/pt/ufp/info/esof/lectures/LecturesApplicationTests.java
@@ -3,7 +3,7 @@ package pt.ufp.info.esof.lectures;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class LecturesApplicationTests {
 
     @Test
