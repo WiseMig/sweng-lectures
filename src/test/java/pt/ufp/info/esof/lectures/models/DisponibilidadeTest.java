ee964223aa7638c1b75698ec1113696545812c3d
Alessandro Miguez <afmoreira@ufp.edu.pt>
2020-11-19 11:37:38 +0000

    removidos imports não utilizados

--- src/test/java/pt/ufp/info/esof/lectures/models/DisponibilidadeTest.java
+++ src/test/java/pt/ufp/info/esof/lectures/models/DisponibilidadeTest.java
@@ -2,7 +2,6 @@ package pt.ufp.info.esof.lectures.models;
 
 import org.junit.jupiter.api.Test;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
